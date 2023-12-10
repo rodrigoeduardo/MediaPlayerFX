0a5c80b89288d67da1898aae59ddfed259fa98d3
rodrigoeduardo <rodrigoow99@hotmail.com>
2023-12-10 18:41:00 -0300

    começa implementação da janela do player

--- src/main/java/br/ufrn/imd/mediaplayerfx/controle/PlayerController.java
+++ src/main/java/br/ufrn/imd/mediaplayerfx/controle/PlayerController.java
@@ -1,2 +1,27 @@
-package br.ufrn.imd.mediaplayerfx.controle;public class PlayerController {
+package br.ufrn.imd.mediaplayerfx.controle;
+
+import br.ufrn.imd.controle.ControleUsuario;
+import br.ufrn.imd.modelo.UsuarioPremium;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class PlayerController {
+    @FXML
+    private Label username;
+    @FXML
+    private Label tipoContaUsuario;
+    @FXML
+    private Button virarPremiumBtn;
+
+    public void start() {
+        ControleUsuario controleUsuario = ControleUsuario.getInstancia();
+
+        username.setText(controleUsuario.getUsuarioLogado().getUsername());
+
+        if (controleUsuario.getUsuarioLogado() instanceof UsuarioPremium) {
+            tipoContaUsuario.setText("Premium");
+            virarPremiumBtn.setDisable(true);
+        }
+    }
 }
