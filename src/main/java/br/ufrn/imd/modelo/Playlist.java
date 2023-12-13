4c11c35113cc398d17abf734a30da4789d124699
LucasNCortez <104558353+LucasNCortez@users.noreply.github.com>
2023-12-13 19:45:39 -0300

    Adição de JavaDOC em modelo e utils

--- src/main/java/br/ufrn/imd/modelo/Playlist.java
+++ src/main/java/br/ufrn/imd/modelo/Playlist.java
@@ -3,31 +3,66 @@ package br.ufrn.imd.modelo;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A classe Playlist representa uma lista de músicas da classe Música, com metódos de adição e remoção para tais.
+ */
 public class Playlist {
+
+  /**
+   * Nome da playlist.
+   */
   private String nome;
+
+  /**
+   * Lista de músicas na playlist.
+   */
   private List<Musica> musicas;
 
+  /**
+   * Construtor da classe Playlist.
+   * @param nome Nome da playlist.
+   */
   public Playlist(String nome) {
     this.nome = nome;
     this.musicas = new ArrayList<>();
   }
 
+  /**
+   * Adiciona uma música à playlist.
+   * @param musica Música a ser adicionada à playlist.
+   */
   public void adicionarMusica(Musica musica) {
     this.musicas.add(musica);
   }
 
+  /**
+   * Remove uma música da playlist.
+   * @param musica Música a ser removida da playlist.
+   */
   public void removerMusica(Musica musica) {
     this.musicas.remove(musica);
   }
 
+  /**
+   * Obtém o nome da playlist.
+   * @return Nome da playlist.
+   */
   public String getNome() {
     return this.nome;
   }
 
+  /**
+   * Define um novo nome para a playlist.
+   * @param novoNome Novo nome da playlist.
+   */
   public void setNome(String novoNome) {
     this.nome = novoNome;
   }
 
+  /**
+   * Obtém a lista de músicas na playlist.
+   * @return Lista de músicas na playlist.
+   */
   public List<Musica> getMusicas() {
     return this.musicas;
   }
--- src/main/java/br/ufrn/imd/modelo/UsuarioPremium.java
+++ src/main/java/br/ufrn/imd/modelo/UsuarioPremium.java
@@ -3,30 +3,62 @@ package br.ufrn.imd.modelo;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A classe UsuarioPremium representa um usuário premium que herda da classe Usuario.
+ * Possui a funcionalidade especial de criar e remover playlists.
+ */
 public class UsuarioPremium extends Usuario {
+
+  /**
+   * Lista de playlists do usuário premium.
+   */
   private List<Playlist> playlists;
 
+  /**
+   * Construtor da classe UsuarioPremium.
+   * @param username Nome de usuário do usuário premium.
+   * @param senha Senha do usuário premium.
+   */
   public UsuarioPremium(String username, String senha) {
     super(username, senha);
     this.playlists = new ArrayList<>();
   }
 
+  /**
+   * Converte um usuário premium para um usuário gratuito.
+   */
   public void virarFree() {
 
   }
 
+  /**
+   * Adiciona uma playlist à lista de playlists do usuário premium.
+   * @param playlist Playlist a ser adicionada.
+   */
   public void adicionarPlaylist(Playlist playlist) {
     this.playlists.add(playlist);
   }
 
+  /**
+   * Remove uma playlist da lista de playlists do usuário premium.
+   * @param playlist Playlist a ser removida.
+   */
   public void removerPlaylist(Playlist playlist) {
     this.playlists.remove(playlist);
   }
 
-   public List<Playlist> getPlaylists() {
+  /**
+   * Obtém a lista de playlists do usuário premium.
+   * @return Lista de playlists do usuário premium.
+   */
+  public List<Playlist> getPlaylists() {
     return this.playlists;
   }
 
+  /**
+   * Define a lista de playlists do usuário premium.
+   * @param playlists A nova lista de playlists do usuário premium.
+   */
   public void setPlaylists(List<Playlist> playlists) {
     this.playlists = playlists;
   }
