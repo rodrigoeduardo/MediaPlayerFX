42f22bb2ebb031cc1107fbf90316606666f5e01a
LucasNCortez <lucascortez6@hotmail.com>
2023-12-13 23:55:26 -0300

    funções de butões adicionadas

--- src/main/java/br/ufrn/imd/mediaplayerfx/controle/PlayerController.java
+++ src/main/java/br/ufrn/imd/mediaplayerfx/controle/PlayerController.java
@@ -3,9 +3,12 @@ package br.ufrn.imd.mediaplayerfx.controle;
 import br.ufrn.imd.controle.ControleUsuario;
 import br.ufrn.imd.modelo.Playlist;
 import br.ufrn.imd.modelo.UsuarioPremium;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.media.MediaPlayer;
 
 public class PlayerController {
@@ -17,6 +20,64 @@ public class PlayerController {
     private Button virarPremiumBtn;
     @FXML
     private ListView<Playlist> playlistsDoUsuario;
+    @FXML
+    private Button btnAddDiretorio;
+
+    @FXML
+    private Button btnAddMusica;
+
+    @FXML
+    private Button btnAddMusicaPlaylist;
+
+    @FXML
+    private Button btnPause;
+
+    @FXML
+    private Button btnPlay;
+
+    @FXML
+    private Button btnRemovePlaylist;
+
+    @FXML
+    private ListView<?> diretorioGeral;
+
+    @FXML
+    private Label lblDuration;
+
+    @FXML
+    private ListView<?> playListAtual;
+
+
+
+    @FXML
+    void btnPause(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnPlay(MouseEvent event) {
+
+    }
+
+    @FXML
+    void continuarMusica(ActionEvent event) {
+
+    }
+
+    @FXML
+    void pausarMusica(ActionEvent event) {
+
+    }
+
+    @FXML
+    void tocarMusicaAnterior(ActionEvent event) {
+
+    }
+
+    @FXML
+    void tocarProximaMusica(ActionEvent event) {
+
+    }
 
     public void start() {
         ControleUsuario controleUsuario = ControleUsuario.getInstancia();
